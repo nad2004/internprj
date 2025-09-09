d15eb94b1cba556186ac854da5baed5ca4e1277c
NAD <anhhuy007hd@gmail.com>
2025-09-09 18:13:54 +0700

    b

--- be/src/server.js
+++ be/src/server.js
@@ -1,5 +1,5 @@
 import cors from 'cors';
-
+import './jobs/deleteUnverifiedUsers.js';
 import express from 'express';
 import dotenv from 'dotenv';
 import connectDB from './config/db.js';
@@ -14,36 +14,16 @@ import notificationRoutes from './routes/notification.routes.js';
 import bookInstanceRoutes from './routes/bookInstance.routes.js';
 import cookieParser from 'cookie-parser';
 import errorHandler from './middleware/errorHandler.js';
-import './jobs/deleteUnverifiedUsers.js';
 dotenv.config();
 const app = express();
 app.use(express.json());
 app.use(cookieParser());
-const allowedOrigins = [
-  "http://localhost:3000",    
-  "https://sneaker-omega.vercel.app" 
-];
-
 app.use(
   cors({
-    origin: function (origin, callback) {
-      // Cho phép request không có origin (Postman, server-to-server)
-      if (!origin) return callback(null, true);
-
-      if (allowedOrigins.includes(origin)) {
-        return callback(null, true);
-      } else {
-        return callback(new Error("Not allowed by CORS"));
-      }
-    },
-    credentials: true, // cho phép cookie, Authorization header
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization"],
-  })
+    origin: ['http://localhost:3000', 'https://sneaker-omega.vercel.app'], 
+    credentials: true,
+  }),
 );
-
-// Cho phép preflight request (OPTIONS)
-app.options("*", cors());
 
 connectDB();
 
@@ -56,7 +36,7 @@ app.use('/api/google', googleRoutes);
 app.use('/api/loan', loanRoutes);
 app.use('/api/upload', uploadRoutes);
 app.use('/api/notify', notificationRoutes);
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT;
 
 app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
 app.use(errorHandler);
