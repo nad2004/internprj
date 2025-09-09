80614cf8ba843ae0841987ae7240f14cfe82d6a5
NAD <anhhuy007hd@gmail.com>
2025-09-09 18:14:41 +0700

    av

--- be/src/server.js
+++ be/src/server.js
@@ -20,7 +20,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: ['http://localhost:3000', 'https://sneaker-omega.vercel.app'], 
+    origin: ['http://localhost:3000', 'https://internprj.vercel.app'], 
     credentials: true,
   }),
 );
