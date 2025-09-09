c34a0586b2eec4c434c7398ad5570497bdc56541
NAD <anhhuy007hd@gmail.com>
2025-09-09 17:36:24 +0700

    fix redis

--- be/src/utils/redis.js
+++ be/src/utils/redis.js
@@ -1,7 +1,11 @@
 import { createClient } from 'redis';
 
 const redisClient = createClient({
-  url: process.env.REDIS_URL || 'redis://localhost:6379',
+  url: process.env.REDIS_URL,
+  socket: {
+    tls: true,
+    rejectUnauthorized: false,
+  },
 });
 
 redisClient.on('error', (err) => console.log('Redis Client Error', err));
